49b5eb02f78dd6f29c1f9179872fc6b31d124f2e
paulofelipefeitosa <paulofelipe.feitosa@gmail.com>
2020-10-01 19:30:39 -0300

    Add choosing ingredients experiment data and analysis script

---
+++ eval/choosing-ingredients/analysis.R
@@ -0,0 +1,113 @@
+# Ambos os csv's (Criu e NoCriu) possuem as seguintes métricas:
+# MainEntry -- Timestamp em nanosegundos do momento em que a app entrou no código Main.
+# MainExit -- Timestamp em nanosegundos do momento em que a app saiu do código Main.
+# Read2Serve -- Timestamp em nanosegundos do momento em que a função da app começou a atender a requisição.
+# RuntimeReadyTime -- Tempo em nanosegundos que levou para a função da app começar a atender a requisição.
+# ServiceTime -- Tempo em nanosegundos que levou para a função da app começar a servir/computar a requisição.
+# LatencyTime -- Tempo em nanosegundos que levou para a app responder a requisição.
+
+# Porém, a execução sem criu (NoCriu) possui as seguintes métricas adicionais:
+
+# LoadedClasses -- Quantidade de classes carregadas na função da app no momento da requisição
+# FindingClassesTime -- Tempo em nanosegundos que a função da app passou apenas encontrando as classes, ou seja, o tempo de lê os dados do arquivo .class e criar o objeto Class<?>.
+# CompilingClassesTime -- Tempo em nanosegundos que a função da app passou apenas criando as instâncias das classes pelo construtor de default (é sempre garantido que ele existe!).
+# LoadClassesTotalTime -- Tempo em nanosegundos que a função da app passou lendo as entradas do jar da função sintética (o nome dos arquivos .class da função sintética gerada), carregando e compilando todas as classes.
+# LoadingClassesOverheadTime -- Tempo em nanosegundos que durante o carregamento das classes a função da app não estava encontrando ou compilando classes, podemos interpretar como (LoadClassesTotalTime - FindingClassesTime - CompilingClassesTime).
+
+require(dplyr)
+require(ggplot2)
+require(reshape)
+require(ggpubr)
+
+read_service_time <- function(f) {
+  df <- read.csv(f)
+  df <- data.frame(
+    Complexity=df[df$Metric=="ServiceTime", ]$Loaded_Classes,
+    Value=df[df$Metric=="ServiceTime", ]$KernelTime_NS+df[df$Metric=="RuntimeReadyTime", ]$KernelTime_NS)
+  colnames(df) <- c("app", "value")
+  df$value <- df$value / 10^6
+  df <- df %>% mutate(app = ifelse(app == "50", "Small", ifelse(app == 250, "Medium", "Big")))
+  return(df)
+}
+pb <- read_service_time("data/criu-nobpf-nogc-warmup-noop-class-loader.csv")
+pb$type <- "Prebaking-Warmup"
+pb1 <- read_service_time("data/criu-nobpf-nogc-nowarmup-noop-class-loader.csv")
+pb1$type <- "Prebaking-NOWarmup"
+vanilla <- read_service_time("data/nocriu-nobpftrace-nogc-nowarmup-noop-class-loader.csv")
+vanilla$type <- "Vanilla"
+
+calculate_median <- function(df, app) {
+  df.t <- wilcox.test(df[df$app == app, ]$value, conf.int = T)
+  return(data.frame(
+    app=app,
+    value=df.t$estimate,
+    min=df.t$conf.int[1],
+    max=df.t$conf.int[2]))
+}
+
+calculate_median(pb, "Small")
+calculate_median(pb, "Medium")
+calculate_median(pb, "Big")
+calculate_median(pb1, "Small")
+calculate_median(pb1, "Medium")
+calculate_median(pb1, "Big")
+calculate_median(vanilla, "Small")
+calculate_median(vanilla, "Medium")
+calculate_median(vanilla, "Big")
+
+calculate_ratio <- function(pb, vanilla, app) {
+  pb.t <- wilcox.test(pb[pb$app == app, ]$value, conf.int = T)
+  vanilla.t <- wilcox.test(vanilla[vanilla$app == app, ]$value, conf.int = T)
+  return(data.frame(
+    type=pb$type,
+    app=app,
+    value=(vanilla.t$estimate/pb.t$estimate)*100,
+    min=(vanilla.t$conf.int[1]/pb.t$conf.int[1])*100,
+    max=(vanilla.t$conf.int[2]/pb.t$conf.int[2])*100))
+}
+
+result <- calculate_ratio(pb, vanilla, "Small")
+result <- rbind(result, calculate_ratio(pb, vanilla, "Medium"))
+result <- rbind(result, calculate_ratio(pb, vanilla, "Big"))
+result <- rbind(result, calculate_ratio(pb1, vanilla, "Small"))
+result <- rbind(result, calculate_ratio(pb1, vanilla, "Medium"))
+result <- rbind(result, calculate_ratio(pb1, vanilla, "Big"))
+
+ggplot(result, aes(x=app, value, fill=type)) +
+  #geom_errorbar(aes(ymin=min, ymax=max), width=.5) +
+  geom_bar(position="dodge", stat = "identity") +
+  theme_pubclean() +
+  labs(y = "Startup Time Improvement (%)", x="Function Size", fill = "Type") +
+  scale_x_discrete(limits=c("Small", "Medium", "Big")) +
+  theme(
+    legend.position="bottom",
+    panel.grid.major.x = element_line(colour = "darkgray", linetype = 3))
+
+ggsave("impact_function_size_cmp.png")
+
+
+calculate_median <- function(df, app) {
+  df.t <- wilcox.test(df[df$app == app, ]$value, conf.int = T)
+  return(data.frame(
+    type=df$type,
+    app=app,
+    value=df.t$estimate,
+    min=df.t$conf.int[1],
+    max=df.t$conf.int[2]))
+}
+
+result <- calculate_median(vanilla, "Small")
+result <- rbind(result, calculate_median(vanilla, "Medium"))
+result <- rbind(result, calculate_median(vanilla, "Big"))
+
+ggplot(result, aes(x=app, y=value)) +
+  geom_bar(position="dodge", stat="identity", fill="violetred4") +
+  geom_errorbar(aes(ymin=min, ymax=max), width=.5) +
+  theme_pubclean() +
+  labs(y = "Startup Time (ms)", x="Function Size") +
+  scale_x_discrete(limits=c("Small", "Medium", "Big")) +
+  theme(
+    legend.position="bottom",
+    panel.grid.major.x = element_line(colour = "darkgray", linetype = 3))
+
+ggsave("impact_function_size_vanilla.png")
